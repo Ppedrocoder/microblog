1b0c63e20775079d1681b3f68a2001fcf05ff792
Pedro <rpedroricardo87@gmail.com>
2026-06-13 23:36:00 -0300

    feat: add routes in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/login'
+import Cadastro from './pages/cadastro'
 import './App.css'
+import Feed from './pages/feed'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route 
+          path="/login"
+          element={<Login/>}
+          />
+          <Route
+          path="/cadastro"
+          element={<Cadastro/>}
+          />
+          <Route
+          path="/"
+          element={<Feed/>}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
