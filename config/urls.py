49f62268f784dd72a57424e97f9b84e2af26eca9
Pedro <rpedroricardo87@gmail.com>
2026-06-14 22:08:57 -0300

    feat: add refresh token route in api

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenRefreshView
 
 import api.urls
 
 urlpatterns = [
     path('', include(api.urls)),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
 ]
 
