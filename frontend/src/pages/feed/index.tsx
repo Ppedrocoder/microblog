6c257361553df1ed3c2f6de880f6ef49f56ab8f8
Pedro <rpedroricardo87@gmail.com>
2026-06-14 22:09:59 -0300

    feat: add logout logic and connect with NavBar component

--- frontend/src/pages/feed/index.tsx
+++ frontend/src/pages/feed/index.tsx
@@ -1,26 +1,18 @@
-import Logo from "../../assets/images/logo-pnp-pura.png"
-import Avatar from "../../assets/images/avatar.svg"
+import { Navigate } from "react-router-dom";
+import NavBar from "../../components/NavBar";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function Feed() {
+    const { logout, isLogged, loading, user } = useAuth();
+    function handleLogout() {
+        logout();
+    }
+    if(!isLogged && !loading) {
+        return <Navigate to="/login" />;
+    }
     return (
-        <>
-            <div className="grid grid-cols-1 w-full h-full">
-                <div className="br-header compact grid-cols-3">
-                    <div className="w-full h-full flex items-center justify-start pl-12">
-                        <button className="br-button small circle" type="button" aria-label="Menu" data-toggle="menu" data-target="#main-navigation" id="menu-compact"><i className="fa fa-bars" aria-hidden="true"></i>
-                        </button>
-                    </div>
-                    <div className="w-full h-full flex items-center justify-center">
-                        <img src={Logo} alt="Microblog" className="w-30 h-14"/>
-                    </div>
-                    <div className="w-full h-full flex items-center justify-end pr-12">
-                        <div className="flex flex-col items-center gap-2 h-full">
-                            <img src={Avatar} alt="Avatar" className="w-10 h-10 rounded-full"/>
-                            <p>Olá, Pedro</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <>  
+            <NavBar action={handleLogout} nome={user?.username || "Usuário"} />
         </>
     )
 }
