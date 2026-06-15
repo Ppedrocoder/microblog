6629c4781f43e0a994d4c1cf9b961fe5c2fc7f15
Pedro <rpedroricardo87@gmail.com>
2026-06-15 11:20:09 -0300

    feat: starting Postagem component

--- frontend/src/components/Postagem/index.tsx
+++ frontend/src/components/Postagem/index.tsx
@@ -1,6 +1,35 @@
-export default function Postagem() {
+
+type PostagemProps = {
+    autor: string;
+    titulo: string;
+    imagem: string;
+    descricao: string;
+    dia: string;
+}
+export default function Postagem(props: PostagemProps) {
     return (
         <>
+            <div className="p-4">
+                <img className="w-104 h-65 border border-transparent rounded-t-2xl" src={props.imagem} alt={props.titulo} />
+                <div className="br-modal medium w-104 justify-center items-center">
+                    <div className="w-full h-full flex flex-col justify-start items-start gap-2 p-4">
+                        <div className="w-full flex flex-row gap-12">  
+                            <div className="w-full grid grid-cols-2 gap-12">
+                                <div className="w-full h-15">   
+                                    <h1 className=" !text-xl !font-bold w-50 mb-0">{props.titulo}</h1>
+                                    <h2 className=" !text-sm w-50 mt-0">{props.autor} - {props.dia}</h2>
+                                </div>
+                                <div className=" w-full h-15 flex justify-end items-start">
+                                    <button className="flex justify-end"><i className="fa fa-comment"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                        <p className="w-full overflow-hidden">{props.descricao}</p>
+                        <span className="border-2 br-divider w-full"></span>
+                    </div>
+                </div>
+            </div>
+            
         </>
     )
 }
