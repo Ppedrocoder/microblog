54684a6bb677f9080b7635003554ac16ffe0ff03
Pedro <rpedroricardo87@gmail.com>
2026-06-19 11:37:10 -0300

    feat: changing BrFooter

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,15 +1,17 @@
-export default function Footer(){
-    return (
-        <>
-            <footer className="br-footer">
-                <div className="container-lg">
-                </div><span className="br-divider my-3"></span>
-                <div className="container-lg">
-                    <div className="info">
-                    <div className="text-down-01 text-medium pb-3">Texto destinado a exibição de informações relacionadas à&nbsp;<strong>licença de uso.</strong></div>
-                    </div>
-                </div>
-            </footer>
-        </>
-    )
+export default function BrFooter() {
+	return (
+		<>
+			<footer className="br-footer">
+				<div className="container-lg"></div>
+				<span className="br-divider my-3"></span>
+				<div className="container-lg">
+					<div className="info">
+						<div className="text-down-01 text-medium pb-3">
+							<strong>PNP Hub</strong>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
 }
