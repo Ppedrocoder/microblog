39ea5d96bf0df4fc6b4b09204fcbfc241d44ee91
Pedro <rpedroricardo87@gmail.com>
2026-06-19 11:38:33 -0300

    feat: add ListaComentarios component

---
+++ frontend/src/components/ListaComentarios/index.tsx
@@ -0,0 +1,33 @@
+import type { ComentarioInterface } from "../../interfaces/ComentarioInterface";
+import formatarDataHora from "../../utils/formatarData";
+
+interface ComentarioProps {
+	comentarios: ComentarioInterface[];
+}
+
+export default function ListaComentarios(props: ComentarioProps) {
+	const { comentarios } = props;
+	return (
+		<>
+			<div>
+				<h3>Comentários</h3>
+				{comentarios.map((comentario: ComentarioInterface) => (
+					<div
+						key={comentario.id}
+						className="w-250"
+					>
+						<div>
+							<h4 className="font-bold !text-lg mb-0">
+								{comentario.autor.username}
+							</h4>
+							<span className="text-sm text-gray-500">
+								{formatarDataHora(comentario.publicado_em)}
+							</span>
+						</div>
+						<p>{comentario.mensagem}</p>
+					</div>
+				))}
+			</div>
+		</>
+	);
+}
